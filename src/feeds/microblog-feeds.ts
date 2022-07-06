340cedf2a9c25f4343e170fc2429d16c53447a81
Keisuke Nakayama <me@yammer.jp>
2022-07-06 21:20:51 +0900

    use async generator

--- src/feeds/microblog-feeds.ts
+++ src/feeds/microblog-feeds.ts
@@ -1,19 +1,17 @@
 import type { JsonFeedItem } from '../types/JsonFeedItem'
-// async function* fetchFeedsIterable(): AsyncGenerator<JsonFeedItem[]> {
-const fetchFeedsIterable = {
-  async *[Symbol.asyncIterator](): AsyncGenerator<JsonFeedItem[]> {
-    let fetchingUrl = 'https://textfeed-api.herokuapp.com/json_feed'
-    while(fetchingUrl) {
-      const responseJson = await fetch(fetchingUrl).then(res =>res.json())
-      fetchingUrl = responseJson.next_url
-      yield (responseJson.items as any[]).map((item: any) => ({ ...item, _site_name: 'microblog'} as JsonFeedItem))
-    }
+
+async function* fetchFeedsGenerator(): AsyncGenerator<JsonFeedItem[]> {
+  let fetchingUrl = 'https://textfeed-api.herokuapp.com/json_feed'
+  while(fetchingUrl) {
+    const responseJson = await fetch(fetchingUrl).then(res =>res.json())
+    fetchingUrl = responseJson.next_url
+    yield (responseJson.items as any[]).map((item: any) => ({ ...item, _site_name: 'microblog'} as JsonFeedItem))
   }
 }
 
 export async function fetchFeeds(): Promise<JsonFeedItem[]> {
   let p: JsonFeedItem[] = [];
-  for await (const f of fetchFeedsIterable) {
+  for await (const f of fetchFeedsGenerator()) {
     p.push(...f);
   }
   return p;
