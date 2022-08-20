6e8b1fc6704ab00fe019dee7aeec05387d6e9ed9
Keisuke Nakayama <me@yammer.jp>
2022-08-20 16:44:48 +0900

    migrate to sveltekit

--- svelte/src/types/JsonFeedItem.ts
+++
@@ -1,9 +0,0 @@
-export interface JsonFeedItem {
-  id: string;
-  url: string;
-  title: string;
-  content_html?: string;
-  content_text?: string;
-  date_published: string;
-  _site_name?: string;
-}
