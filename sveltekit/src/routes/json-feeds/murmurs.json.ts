83fce7c8c3570fbf45992fa96586b3c2b153a4e4
Keisuke Nakayama <me@yammer.jp>
2022-10-11 08:34:29 +0900

    migrate murmurs hostname

--- sveltekit/src/routes/json-feeds/murmurs.json.ts
+++ sveltekit/src/routes/json-feeds/murmurs.json.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { fetchFeeds } from '$lib/feedsManagement/JsonFeedFetcher'
 
-const feedUrl = 'https://textfeed-api.herokuapp.com/json_feed';
+const feedUrl = 'https://textfeed-api.yammer.jp/json_feed';
 
 export const GET: RequestHandler = async () => {
   const feeds = await fetchFeeds(feedUrl)
